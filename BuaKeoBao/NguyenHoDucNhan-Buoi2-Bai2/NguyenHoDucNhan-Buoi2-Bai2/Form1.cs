c0f5bc135d45deaf151e6ac327a1fe4ab344979a
Đức Nhân <avenged.king@gmail.com>
2017-10-13 23:15:11 +0700

    Kéo - Búa - Bao

---
+++ BuaKeoBao/NguyenHoDucNhan-Buoi2-Bai2/NguyenHoDucNhan-Buoi2-Bai2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+namespace NguyenHoDucNhan_Buoi2_Bai2
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        IPEndPoint ipep;
+        EndPoint remote;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            remote = (EndPoint)ipep;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Kéo";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, remote);
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Búa";
+            int b = 1;
+            byte[] send = BitConverter.GetBytes(b);
+            client.SendTo(send, remote);
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            int c = 2;
+            byte[] send = BitConverter.GetBytes(c);
+            client.SendTo(send, remote);
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+    }
+}
---
+++ BuaKeoBao/NguyenHoDucNhan-Buoi2-Bai2/Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            server.Bind(ipep);
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+
+            byte[] data = new byte[4];
+            server.ReceiveFrom(data, ref Remote);
+            int clientChoosen = BitConverter.ToInt32(data, 0);
+            int serverChoosen;
+            Random rand = new Random();
+            serverChoosen = rand.Next(0, 2);
+            if (clientChoosen == serverChoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Hòa");
+                server.SendTo(send, Remote);
+            }
+            else if (clientChoosen == 0 && serverChoosen == 1)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+            else if (clientChoosen == 0 && serverChoosen == 2)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thắng");
+                server.SendTo(send, Remote);
+            }
+            else if (clientChoosen == 1 && serverChoosen == 00)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thắng");
+                server.SendTo(send, Remote);
+            }
+            else if (clientChoosen == 1 && serverChoosen == 2)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+            else if (clientChoosen == 2 && serverChoosen == 0)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, Remote);
+            }
+            else if (clientChoosen == 2 && serverChoosen == 1)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thắng");
+                server.SendTo(send, Remote);
+            }
+        }
+    }
+}
